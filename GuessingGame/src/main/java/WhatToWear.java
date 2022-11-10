cb2845bffcaf9338a3767788ff33e740043b2e11
amylianne <amywynn.3.a@gmail.com>
2022-11-10 19:23:10 +0000

    added user inputs

--- GuessingGame/src/main/java/WhatToWear.java
+++ GuessingGame/src/main/java/WhatToWear.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class WhatToWear {
     public static void main(String[] args) {
-        String weather = "sunny";
-        int temp = 20;
+
+//        String weather = "sunny";
+//        int temp = 20;
+// prompt user for input on weather
+        System.out.println("what is the weather like today?");
+        // collect user input weather
+        Scanner reader = new Scanner(System.in);
+        String weather = reader.nextLine();
+
+        //input for temperature
+        System.out.println("what is the temperature like today?");
+        // collect user input temp
+        Scanner reader2 = new Scanner(System.in);
+        int temp = reader2.nextInt();
 
         //weather="rainy" && temp < 15) {
         if (weather.equals("rainy") && temp<20){
@@ -18,7 +31,9 @@ public class WhatToWear {
         else if (weather.equals("sunny") && temp>20){
             System.out.println("No need for a coat");
         }
-
+        else {
+            System.out.println("Hmmmm not sure");
+        }
 
     }
 }
